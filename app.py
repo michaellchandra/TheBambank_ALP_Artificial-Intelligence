8a8801bab6f5ac541c987eedba0b09961dba9725
michaellchandra <chandramichaell93@gmail.com>
2022-06-11 21:39:31 +0700

    UI updated

--- app.py
+++ app.py
@@ -13,5 +13,24 @@ def home():
         "index.html",
         name = "Home - The Bambank Color Palette"
     )
+@app.route("/home")
+def homee():
+    return render_template(
+        "index.html",
+        name = "Home - The Bambank Color Palette"
+    )
+    
+@app.route("/about")
+def about():
+    return render_template(
+        "about.html",
+        name = "About Us - The Bambank Color Palette"
+    )
 
+@app.route("/colorpalette")
+def colorpalette():
+    return render_template(
+        "colorPalette.html",
+        name = "Color Palette - The Bambank Color Palette"
+    )
 
